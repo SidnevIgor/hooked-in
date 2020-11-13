0c9538e5f80ae8f27a0b5ef899ec9cea1b7f7733
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-11-13 10:31:11 +0300

    Fixing navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -6,8 +6,11 @@ export default class Navbar extends Component {
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
-            <li className="nav-item active">
-              <a className="nav-link" href="#">Home</a>
+            <li className="nav-item">
+              <a className="nav-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer">Home</a>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="#">Features</a>
