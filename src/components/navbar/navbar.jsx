f08ebcd717c454354766f41065c8b56aaeb3d2e5
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-11-13 09:02:55 +0300

    Update navbar.jsx

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -4,14 +4,10 @@ export default class Navbar extends Component {
   render() {
     return (
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">Navbar</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item active">
-              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+              <a className="nav-link" href="#">Home</a>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="#">Features</a>
@@ -20,7 +16,7 @@ export default class Navbar extends Component {
               <a className="nav-link" href="#">Pricing</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link disabled" href="#">Disabled</a>
+              <a className="nav-link" href="#">Disabled</a>
             </li>
           </ul>
         </div>
